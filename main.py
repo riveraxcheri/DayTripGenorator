0ce74f0259a6bf315ef570927cc159dc9a1d0447
riveraxcheri <117052183+riveraxcheri@users.noreply.github.com>
2022-10-31 00:06:42 -0500

    Add in Generator Functions
    Add functions to print, determine satisfaction, and 1st attempt at reselect option.

--- main.py
+++ main.py
@@ -11,22 +11,40 @@ def select_destination():
     selected_destination = random.choice(destinations)
     return selected_destination
 
-print (select_destination())
-
 def select_restaurant():
     selected_restaurant = random.choice(restaurants)
     return selected_restaurant
-
-print (select_restaurant())
 
 def select_entertainment():
     selected_entertainment = random.choice(entertainment)
     return selected_entertainment
 
-print (select_entertainment())
-
 def select_transportation():
     selected_transport = random.choice(transportation)
     return selected_transport
 
-print (select_transportation())
+def print_full_trip():
+    print ("Here is your trip:")
+    print (f"Destination: {select_destination()}")
+    print (f"Restaurant: {select_restaurant()}")
+    print (f"Entertainment: {select_entertainment()}")
+    print (f"Transportation: {select_transportation()}")
+
+def determine_satisfaction():
+    user_input = input("Are you satisfied with your trip? (Y/N) ")
+    if user_input is "Y":
+        print (f"Great! Here is your final trip: {print_full_trip()}")
+    else:
+        print (generate_new())
+
+def generate_new():
+    user_input = input("What part of your trip would you like to change? ")
+    if user_input is "Destination":
+        new_destination = random.choice(destinations.remove(select_destination())) 
+        print (new_destination)         
+
+print_full_trip()
+
+determine_satisfaction()
+
+  
