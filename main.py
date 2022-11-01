ec720d8c4a12e8bcf00050d9aa1af369b74c795c
riveraxcheri <117052183+riveraxcheri@users.noreply.github.com>
2022-11-01 00:50:55 -0500

    Add and update functions
    add and update functions to create an easy flow to reselect from each list. brainstorming how to get remove function to work

--- main.py
+++ main.py
@@ -7,44 +7,67 @@ restaurants = ["The Three Broomsticks", "Honeydukes", "The Leaky Cauldron", "Hog
 entertainment = ["A Quidditch Match", "The Triwizard Tournament", "Wizard's Chess", "Charms Class"]
 transportation = ["The Hogwarts Express", "The Knight Bus", "Broomstick", "Flying Car", "Portkey"]
 
-def select_destination():
-    selected_destination = random.choice(destinations)
+def select_destination(destination_list):
+    selected_destination = random.choice(destination_list)
     return selected_destination
 
-def select_restaurant():
-    selected_restaurant = random.choice(restaurants)
+user_destination = select_destination(destinations)
+
+def reselect_destination(destination_list):
+    new_destination = random.choice(destination_list.remove(user_destination))
+    return new_destination
+
+def select_restaurant(restaurant_list):
+    selected_restaurant = random.choice(restaurant_list)
     return selected_restaurant
 
-def select_entertainment():
-    selected_entertainment = random.choice(entertainment)
+user_restaurant = select_restaurant(restaurants)
+
+def select_entertainment(entertainment_list):
+    selected_entertainment = random.choice(entertainment_list)
     return selected_entertainment
 
-def select_transportation():
-    selected_transport = random.choice(transportation)
+user_entertainment = select_entertainment(entertainment)
+
+def select_transportation(transportation_list):
+    selected_transport = random.choice(transportation_list)
     return selected_transport
 
-def print_full_trip():
-    print ("Here is your trip:")
-    print (f"Destination: {select_destination()}")
-    print (f"Restaurant: {select_restaurant()}")
-    print (f"Entertainment: {select_entertainment()}")
-    print (f"Transportation: {select_transportation()}")
+user_transportation = select_transportation(transportation)
 
-def determine_satisfaction():
-    user_input = input("Are you satisfied with your trip? (Y/N) ")
-    if user_input is "Y":
-        print (f"Great! Here is your final trip: {print_full_trip()}")
-    else:
-        print (generate_new())
+def greeting():
+    print ("Welcome to the Hogwarts Day Trip Generator! Here are your trip details:")
 
-def generate_new():
+def display_full_trip():
+    print (f"Destination: {user_destination}")
+    print (f"Restaurant: {user_restaurant}")
+    print (f"Entertainment: {user_entertainment}")
+    print (f"Transportation: {user_transportation}")
+
+def determine_satisfaction(): 
+    confirm_bool = True
+    while confirm_bool:
+        user_input = input("Are you satisfied with your trip? (y/n) ")
+        if user_input == "n":
+            reselect_item()
+        else:
+            confirm_bool = False
+    return final_msg()
+
+def final_msg():
+    print ("Great! Here are your final trip details:")
+    print (display_full_trip())
+    print ("Enjoy your day!")
+
+def reselect_item():
     user_input = input("What part of your trip would you like to change? ")
-    if user_input is "Destination":
-        new_destination = random.choice(destinations.remove(select_destination())) 
-        print (new_destination)         
+    if user_input == "Destination":
+        reselect_destination()
+    else:
+        print ("Please type in a valid trip detail.")    
 
-print_full_trip()
-
+greeting()
+display_full_trip()
 determine_satisfaction()
 
   
