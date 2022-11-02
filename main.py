b408e82bc247f779e579de75999957ac532839f1
riveraxcheri <117052183+riveraxcheri@users.noreply.github.com>
2022-11-02 12:34:51 -0500

    adjuste code to easy mode
    change the generator to choose a whole new trip each time they are unsatisfied instead of trying to single out and change only one detail of the trip

--- main.py
+++ main.py
@@ -1,73 +1,104 @@
-# Day Trip Generator- Harry Potter
+#Day Trip Generator- Harry Potter
+
 
 import random
+
+#Greeting
+
+def greeting():
+    print ("""
+    Welcome to the Hogwarts Day Trip Generator!
+
+    Here are your trip details:""")
+
+# Lists
 
 destinations = ["Hogwarts", "The Ministry of Magic", "Hogsmeade", "Diagon Alley"]
 restaurants = ["The Three Broomsticks", "Honeydukes", "The Leaky Cauldron", "Hogwarts Dinning Hall"]
 entertainment = ["A Quidditch Match", "The Triwizard Tournament", "Wizard's Chess", "Charms Class"]
 transportation = ["The Hogwarts Express", "The Knight Bus", "Broomstick", "Flying Car", "Portkey"]
 
+## Destinations
 def select_destination(destination_list):
     selected_destination = random.choice(destination_list)
     return selected_destination
 
 user_destination = select_destination(destinations)
 
-def reselect_destination(destination_list):
-    new_destination = random.choice(destination_list.remove(user_destination))
-    return new_destination
-
+## Restaurants
 def select_restaurant(restaurant_list):
     selected_restaurant = random.choice(restaurant_list)
     return selected_restaurant
 
 user_restaurant = select_restaurant(restaurants)
 
+## Entertainment
 def select_entertainment(entertainment_list):
     selected_entertainment = random.choice(entertainment_list)
     return selected_entertainment
 
 user_entertainment = select_entertainment(entertainment)
 
+## Transportation
 def select_transportation(transportation_list):
     selected_transport = random.choice(transportation_list)
     return selected_transport
 
 user_transportation = select_transportation(transportation)
 
-def greeting():
-    print ("Welcome to the Hogwarts Day Trip Generator! Here are your trip details:")
+def reselect_destination(destination_list):
+    return (random.choice(destination_list))
 
-def display_full_trip():
-    print (f"Destination: {user_destination}")
-    print (f"Restaurant: {user_restaurant}")
-    print (f"Entertainment: {user_entertainment}")
-    print (f"Transportation: {user_transportation}")
+# Regenerate specific items on trip details 
+# def reselect_item():
+#     user_input = input("What part of your trip would you like to change? ")
+#     if user_input == "Destination":
+#         destinations.remove(user_destination)
+#         user_destination = reselect_destination(destinations)
+#         print (gen_full_trip())
+#         determine_satisfaction()   
+#     else:
+#         print ("Please type in a valid trip detail.")   
+
+#Trip Details
+
+def gen_full_trip():
+    return (f"""
+    Destination: {select_destination(destinations)}
+    Restaurant: {select_restaurant(restaurants)}
+    Entertainment: {select_entertainment(entertainment)}
+    Transportation: {select_transportation(transportation)}
+    """)
 
 def determine_satisfaction(): 
     confirm_bool = True
     while confirm_bool:
         user_input = input("Are you satisfied with your trip? (y/n) ")
         if user_input == "n":
-            reselect_item()
+            print (gen_full_trip()) and determine_satisfaction()
         else:
             confirm_bool = False
     return final_msg()
 
-def final_msg():
-    print ("Great! Here are your final trip details:")
-    print (display_full_trip())
-    print ("Enjoy your day!")
+# def display_new_trip():
+#     (f"""
+#     Destination:{reselect_destination(destinations)}
+#     Restaurant:
+#     Entertainment:
+#     Transportation:
+#     """)
 
-def reselect_item():
-    user_input = input("What part of your trip would you like to change? ")
-    if user_input == "Destination":
-        reselect_destination()
-    else:
-        print ("Please type in a valid trip detail.")    
+# Ending
+
+def final_msg():
+    print (f"""
+    Thank you for traveling with us!
+    Enjoy your day!""")
+
+# Calling the functions
 
 greeting()
-display_full_trip()
+print (gen_full_trip())
 determine_satisfaction()
 
-  
+
